90519fb0edba86193fb13184c2fcffafcecb746e
JoyceJoaquin <96128076+JoyceJoaquin@users.noreply.github.com>
2021-12-14 09:10:55 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,29 +1,53 @@
-let mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . b 5 b . . . 
-    . . . . . . . . . b 5 b . . . . 
-    . . . . . . b b b b b b . . . . 
-    . . . . . b b 5 5 5 5 5 b . . . 
-    . b b b b b 5 5 5 5 5 5 5 b . . 
-    . b d 5 b 5 5 5 5 5 5 5 5 b . . 
-    . . b 5 5 b 5 d 1 f 5 d 4 f . . 
-    . . b d 5 5 b 1 f f 5 4 4 c . . 
-    b b d b 5 5 5 d f b 4 4 4 4 4 b 
-    b d d c d 5 5 b 5 4 4 4 4 4 b . 
-    c d d d c c b 5 5 5 5 5 5 5 b . 
-    c b d d d d d 5 5 5 5 5 5 5 b . 
-    . c d d d d d d 5 5 5 5 5 d b . 
-    . . c b d d d d d 5 5 5 b b . . 
-    . . . c c c c c c c c b b . . . 
-    `, SpriteKind.Player)
-while (Math.percentChance(50)) {
-    music.playMelody("E B C5 A B G A F ", 120)
-}
-let MAGICWORD = game.askForString("what is the magic word?")
-if (MAGICWORD == "expulso" || MAGICWORD == "******") {
-    game.splash("Login Successful")
-} else {
-    if (true) {
-        game.splash("Login Failed!!")
-    }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+    projectile.destroy(effects.disintegrate, 500)
+})
+scene.onHitWall(SpriteKind.Projectile, function (sprite, location) {
+    info.changeLifeBy(-1)
+    projectile.destroy(effects.disintegrate, 500)
+})
+let mySprite2: Sprite = null
+let projectile: Sprite = null
+info.setScore(0)
+info.setLife(3)
+tiles.setWallAt(tiles.getTileLocation(0, 0), true)
+if (info.life() > 0) {
+    projectile = sprites.createProjectileFromSide(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . b 5 5 b . . . 
+        . . . . . . b b b b b b . . . . 
+        . . . . . b b 5 5 5 5 5 b . . . 
+        . b b b b b 5 5 5 5 5 5 5 b . . 
+        . b d 5 b 5 5 5 5 5 5 5 5 b . . 
+        . . b 5 5 b 5 d 1 f 5 d 4 f . . 
+        . . b d 5 5 b 1 f f 5 4 4 c . . 
+        b b d b 5 5 5 d f b 4 4 4 4 b . 
+        b d d c d 5 5 b 5 4 4 4 4 4 4 b 
+        c d d d c c b 5 5 5 5 5 5 5 b . 
+        c b d d d d d 5 5 5 5 5 5 5 b . 
+        . c d d d d d d 5 5 5 5 5 d b . 
+        . . c b d d d d d 5 5 5 b b . . 
+        . . . c c c c c c c c b b . . . 
+        `, 0, 50)
+    mySprite2 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        e . . . . . . . . . . . . . . e 
+        e . . . . . . . . . . . . . . e 
+        e . . . . . . . . . . . . . . e 
+        e . . . . . . . . . . . . . . e 
+        e . . . . . . . . . . . . . . e 
+        e . . . . . . . . . . . . . . e 
+        e . . . . . . . . . . . . . . e 
+        e e e e e e e e e e e e e e e e 
+        `, SpriteKind.Player)
+    mySprite2.setPosition(76, 111)
+    controller.moveSprite(mySprite2, 100, 0)
 }
